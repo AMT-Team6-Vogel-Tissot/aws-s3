c0d5357db6682763d416e814e1d46f4f1ac545f2
maelle <maelle.vogel@heig-vd.ch>
2022-12-06 17:17:40 +0100

    ajout des méthodes pour les fonctions get

--- src/main/java/heig/vd/s3/controller/api/AppController.java
+++ src/main/java/heig/vd/s3/controller/api/AppController.java
@@ -1,11 +1,14 @@
 package heig.vd.s3.controller.api;
 
+import heig.vd.s3.AwsCloudClient;
 import heig.vd.s3.controller.request.AppRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URL;
 
 
 @RestController
@@ -16,13 +19,13 @@ public class AppController {
         return true;
     }
     @GetMapping("/demandeObjet")
-    public String urlObject(@RequestParam String name) {
-        return "";
+    public URL urlObject(@RequestParam String name) {
+        return (AwsCloudClient.getInstance().getDataObject()).publish(name);
     }
-    //TODO retourner url si object créé
     @PostMapping("/ajoutObjet")
-    public String createObject(@RequestBody AppRequest request) {
-        return "";
+    public boolean createObject(@RequestBody AppRequest request) {
+        AwsCloudClient.getInstance().getDataObject().create(request.getObjectName(),request.getContentFile());
+        return true;
     }
 
 }
